f1e180ea7744a46bed53755743524d0beda84223
tutajm <mtutaj@mcw.edu>
2023-01-31 14:44:36 -0600

    markers: removed generation of RatMine files due to RatMine discontinuation

--- src/main/java/edu/mcw/rgd/gff3/CreateGff4SSLP.java
+++ src/main/java/edu/mcw/rgd/gff3/CreateGff4SSLP.java
@@ -6,11 +6,7 @@ import edu.mcw.rgd.process.Utils;
 import edu.mcw.rgd.process.mapping.MapManager;
 
 import java.io.BufferedWriter;
-import java.io.PrintWriter;
 import java.util.*;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author pjayaraman
@@ -52,9 +48,6 @@ public class CreateGff4SSLP {
         gff3Writer.print("# Primary Contact: mtutaj@mcw.edu\n");
         gff3Writer.print("# Generated: "+new Date()+"\n");
 
-        Gff3ColumnWriter RATMINEgff3Writer = new Gff3ColumnWriter(info.getToDir()+"/RATMINE_"+assemblySymbol+"_markers.gff3", false, info.isCompress());
-        RATMINEgff3Writer.setRatmineCompatibleFormat(true);
-
         // set up writer to create fasta sequences of primer pairs..
         String fastaFileName = info.getToDir()+"/"+assemblySymbol+"_markers.fa";
         if( info.isCompress() ) {
@@ -63,14 +56,13 @@ public class CreateGff4SSLP {
         BufferedWriter fastaWriterFile = Utils.openWriter(fastaFileName);
         FastaWriter fastaWriter = new FastaWriter(fastaWriterFile);
 
-        createGff4Markers(info.getMapKey(), info.getSpeciesTypeKey(), gff3Writer, RATMINEgff3Writer, fastaWriter, counters);
+        createGff4Markers(info.getMapKey(), info.getSpeciesTypeKey(), gff3Writer, fastaWriter, counters);
 
         gff3Writer.close();
-        RATMINEgff3Writer.close();
         fastaWriterFile.close();
     }
 
-    public void createGff4Markers(int mapKey, int speciesTypeKey, Gff3ColumnWriter gff3Writer, Gff3ColumnWriter RATMINEgff3Writer, FastaWriter fastaWriter, CounterPool counters) throws Exception{
+    public void createGff4Markers(int mapKey, int speciesTypeKey, Gff3ColumnWriter gff3Writer, FastaWriter fastaWriter, CounterPool counters) throws Exception{
 
         int countSslps=0;
         int mapSslpCount=0;
@@ -107,58 +99,45 @@ public class CreateGff4SSLP {
             List<MapData> sslpsMapdataList = dao.getMapData(sslpRgdId, mapKey);
 
             if(sslpsMapdataList.size()==0){
-             noMapSslpPos++;
+                noMapSslpPos++;
             }else if(sslpsMapdataList.size()>1){
-             moreThanOneMapSslpPos++;
+                moreThanOneMapSslpPos++;
             }
 
             fastaWriter.writeFastaSequences("forward_"+sslpRgdId, sslp.getForwardSeq(), "reverse_"+sslpRgdId, sslp.getReverseSeq());
 
             if(sslpsMapdataList.size()>0){
 
-             mapSslpCount++;
+                mapSslpCount++;
 
-             for(MapData md: sslpsMapdataList){
-                String chrom = md.getChromosome();
-                Integer start = md.getStartPos();
-                Integer stop = md.getStopPos();
-                String strand = md.getStrand();
-                if(strand==null){
-                    strand = ".";
+                for(MapData md: sslpsMapdataList){
+                    String chrom = md.getChromosome();
+                    Integer start = md.getStartPos();
+                    Integer stop = md.getStopPos();
+                    String strand = md.getStrand();
+                    if(strand==null){
+                        strand = ".";
+                    }
+
+                    gff3Writer.writeFirst8Columns(chrom,"RGD","SSLPS",start,stop,".",strand,".");
+
+                    //initialize hashmap for attributes
+                    HashMap<String, String> attributesHashMap = new HashMap<>();
+
+                    attributesHashMap.put("ID", sslpRgdId+"_"+start+"_"+stop);
+                    attributesHashMap.put("Name", "SSLP:"+sslpSymbol);
+                    if( aliases!=null )
+                        attributesHashMap.put("Alias", aliases+","+"RGD:"+sslpRgdId+","+sslpRgdId+","+sslpSymbol);
+                     else
+                        attributesHashMap.put("Alias", "RGD:"+sslpRgdId+","+sslpRgdId+","+sslpSymbol);
+
+                    attributesHashMap.put("Dbxref", "RGD:"+sslpRgdId);
+                    attributesHashMap.put("expectedSize",expectedSize);
+                    attributesHashMap.put("associatedGene", assocGeneRgdID);
+
+                    gff3Writer.writeAttributes4Gff3(attributesHashMap);
+
                 }
-
-                gff3Writer.writeFirst8Columns(chrom,"RGD","SSLPS",start,stop,".",strand,".");
-                RATMINEgff3Writer.writeFirst8Columns(chrom,"RGD","SimpleSequenceLengthVariation",start,stop,".",strand,".");
-
-                //initialize hashmap for attributes
-                HashMap<String, String> attributesHashMap = new HashMap<>();
-                HashMap<String, String> RATMINEattributesHashMap = new HashMap<>();
-
-                attributesHashMap.put("ID", sslpRgdId+"_"+start+"_"+stop);
-                attributesHashMap.put("Name", "SSLP:"+sslpSymbol);
-                if( aliases!=null )
-                    attributesHashMap.put("Alias", aliases+","+"RGD:"+sslpRgdId+","+sslpRgdId+","+sslpSymbol);
-                 else
-                    attributesHashMap.put("Alias", "RGD:"+sslpRgdId+","+sslpRgdId+","+sslpSymbol);
-
-                attributesHashMap.put("Dbxref", "RGD:"+sslpRgdId);
-                attributesHashMap.put("expectedSize",expectedSize);
-                attributesHashMap.put("associatedGene", assocGeneRgdID);
-
-                gff3Writer.writeAttributes4Gff3(attributesHashMap);
-
-                RATMINEattributesHashMap.put("ID", String.valueOf(sslpRgdId));
-                RATMINEattributesHashMap.put("Name", sslpSymbol);
-                 if( aliases!=null )
-                    RATMINEattributesHashMap.put("Alias", aliases+","+"RGD:"+sslpRgdId+","+sslpRgdId);
-                 else
-                    RATMINEattributesHashMap.put("Alias", "RGD:"+sslpRgdId+","+sslpRgdId);
-                RATMINEattributesHashMap.put("Dbxref", "RGD:"+sslpRgdId);
-                RATMINEattributesHashMap.put("expectedSize",expectedSize);
-                RATMINEattributesHashMap.put("associatedGene", assocGeneRgdID);
-
-                RATMINEgff3Writer.writeAttributes4Gff3(RATMINEattributesHashMap);
-            }//end for each map data list loop
             }
         }
 
