a60c649e2463107798f8600031fc25de18676835
llamersmcw <llamers@mcw.edu>
2023-04-17 08:52:48 -0500

    updated for eva release 4

--- src/main/dist/jbrowse_tracks/eva.sh
+++ src/main/dist/jbrowse_tracks/eva.sh
@@ -38,7 +38,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat72_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_rn7_2 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -59,7 +59,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat60_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_rgd6 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -80,7 +80,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat50_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_rgd5 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -101,7 +101,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/mouse39_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_mm39 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -122,7 +122,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/mouse38_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_mm38 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -143,7 +143,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/dog31_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_dog3_1 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -164,7 +164,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/pig11_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_pig11_1 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -185,7 +185,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/pig10_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_pig10_2 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -206,7 +206,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/grnMonkey1_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 3" \
+      --key "EVA Release 4" \
       --out /jbrowse/data_chlSab2 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
