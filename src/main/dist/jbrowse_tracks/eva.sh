d3d2e987325cb2be069b61b728d6ac458d903dbc
llamersmcw <llamers@mcw.edu>
2024-11-04 13:29:49 -0600

    added EVA load for new dog assembly

--- src/main/dist/jbrowse_tracks/eva.sh
+++ src/main/dist/jbrowse_tracks/eva.sh
@@ -3,7 +3,7 @@
 
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAILLIST=mtutaj@mcw.edu,llamers@mcw.edu
-JBROWSE_HOME="/rgd/JBrowse-1.16.11/"
+JBROWSE_HOME="/rgd/JBrowse-1.16.3/"
 
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 if [ "$SERVER" == "HANSEN" ]; then
@@ -15,6 +15,7 @@ if [ "$SERVER" == "HANSEN" ]; then
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_GRCm39.gff3.gz .
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_GRCm38.gff3.gz .
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_CanFam3.1.gff3.gz .
+    scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_ROS_Cfam_1.0.gff3.gz .
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_Sscrofa11.1.gff3.gz .
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_Sscrofa10.2.gff3.gz .
     scp -p rgddata@travis.rgd.mcw.edu:/home/rgddata/pipelines/RGDGff3Pipeline/data/Eva/EVA_ChlSab1.1.gff3.gz .
@@ -38,7 +39,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat72_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 4" \
+      --key "EVA Release 6" \
       --out /jbrowse/data_rn7_2 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -59,7 +60,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat60_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 4" \
+      --key "EVA Release 6" \
       --out /jbrowse/data_rgd6 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -80,7 +81,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/rat50_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 4" \
+      --key "EVA Release 6" \
       --out /jbrowse/data_rgd5 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -101,7 +102,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/mouse39_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 4" \
+      --key "EVA Release 6" \
       --out /jbrowse/data_mm39 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -122,7 +123,7 @@ if [ -f $TMP_INPUT_FILE ]; then
     ./flatfile-to-json.pl \
       --gff /tmp/mouse38_EVA.gff3 \
       --trackLabel EVA \
-      --key "EVA Release 4" \
+      --key "EVA Release 6" \
       --out /jbrowse/data_mm38 \
       --trackType JBrowse/View/Track/CanvasFeatures \
       --config "{ \"category\" : \"Variants/DbSNPs\" }"
@@ -132,7 +133,7 @@ fi
 
 
 echo
-echo "DOG"
+echo "DOG CanFam3"
 
 TMP_INPUT_FILE=$GFF3_LOC/EVA_CanFam3.1.gff3.gz
 if [ -f $TMP_INPUT_FILE ]; then
@@ -151,6 +152,25 @@ else
     echo "ERROR: File not found: $TMP_INPUT_FILE" |  mailx -s "[$SERVER] GFF3 JBrowse Loader: missing file $TMP_INPUT_FILE" $EMAILLIST
 fi
 
+echo
+echo "DOG ROSCFam1"
+
+TMP_INPUT_FILE=$GFF3_LOC/EVA_ROS_CFam_1.0.gff3.gz
+if [ -f $TMP_INPUT_FILE ]; then
+    gunzip -c $TMP_INPUT_FILE > /tmp/dogROSCFam1_EVA.gff3
+
+    ./remove-track.pl --dir /jbrowse/data_rosCfam1 --trackLabel EVA --delete
+
+    ./flatfile-to-json.pl \
+      --gff /tmp/dogROSCFam1_EVA.gff3 \
+      --trackLabel EVA \
+      --key "EVA Release 6" \
+      --out /jbrowse/data_rosCfam1 \
+      --trackType JBrowse/View/Track/CanvasFeatures \
+      --config "{ \"category\" : \"Variants/DbSNPs\" }"
+else
+    echo "ERROR: File not found: $TMP_INPUT_FILE" |  mailx -s "[$SERVER] GFF3 JBrowse Loader: missing file $TMP_INPUT_FILE" $EMAILLIST
+fi
 
 echo
 echo "PIG 11.1"
