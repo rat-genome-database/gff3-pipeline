67a6f8184e9dec4fe53ba121c3c8e4666e5500c6
llamersmcw <llamers@mcw.edu>
2026-02-06 14:49:51 -0600

    creating script for rat strain specific variants

---
+++ src/main/dist/CreateRatStrainSpecificVariantGff3.sh
@@ -0,0 +1,46 @@
+# script env setup
+#
+. /etc/profile
+
+APP_HOME=/home/rgddata/pipelines/RGDGff3Pipeline/
+OUTDIR=$APP_HOME/data/strain_specific_variants
+#EMAILLIST=jrsmith@mcw.edu,RGD.Developers@mcw.edu
+EMAILLIST="mtutaj@mcw.edu llamers@mcw.edu"
+
+CHR_RAT="1-20,X,Y,MT"
+
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
+
+RUNLOAD="$APP_HOME/run.sh"
+
+cd $APP_HOME
+
+# GRCr8 samples
+LOGFILE=cron_ratcn8.log
+rm $LOGFILE
+$RUNLOAD -patientID:380 -source:CN -toFile:$OUTDIR/rat8/ -chr:$CHR_RAT -compress  2>&1 | tee -a $LOGFILE
+mailx -s "[$SERVER]Pipeline to create Gff3 data for ratcn 8 ran" $EMAILLIST<$LOGFILE
+
+# mRatBN7.2 samples
+LOGFILE=cron_ratcn72.log
+rm $LOGFILE
+$RUNLOAD -patientID:372 -source:CN -toFile:$OUTDIR/rat7/ -chr:$CHR_RAT -compress  2>&1 | tee -a $LOGFILE
+mailx -s "[$SERVER]Pipeline to create Gff3 data for ratcn 7.2 ran" $EMAILLIST<$LOGFILE
+
+# Rnor6.0 samples
+LOGFILE=cron_ratcn60.log
+rm $LOGFILE
+$RUNLOAD -patientID:600 -source:CN -toFile:$OUTDIR/rat6/ -chr:$CHR_RAT -compress  2>&1 | tee -a $LOGFILE
+mailx -s "[$SERVER]Pipeline to create Gff3 data for ratcn 6.0 ran" $EMAILLIST<$LOGFILE
+
+# Rnor5.0 samples
+LOGFILE=cron_ratcn50.log
+rm $LOGFILE
+$RUNLOAD -patientID:500 -source:CN -toFile:$OUTDIR/rat5/ -chr:$CHR_RAT -compress  2>&1 | tee -a $LOGFILE
+mailx -s "[$SERVER]Pipeline to create Gff3 data for ratcn 5.0 ran" $EMAILLIST<$LOGFILE
+
+# RGSC3.4 samples
+LOGFILE=cron_ratcn34.log
+rm $LOGFILE
+$RUNLOAD -patientID:180 -source:CN -toFile:$OUTDIR/rat3_4/ -chr:$CHR_RAT -compress  2>&1 | tee -a $LOGFILE
+mailx -s "[$SERVER]Pipeline to create Gff3 data for ratcn 3.4 ran" $EMAILLIST<$LOGFILE
